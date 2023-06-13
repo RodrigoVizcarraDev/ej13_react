d73abc806df0bc6ab0d12d782b866647fa008d15
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-06-13 01:58:57 -0300

    Codigo depurado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -22,17 +22,13 @@ const Formulario = ({ kelvin_to_celsius }) => {
                 },arg&limit=1&appid=8088c068d388132744ac36cb925485fe`
             );
             const datos = await peticionAPI.json();
-            console.log(datos);
-            console.log(datos[0].lat);
             const latitud = datos[0].lat;
             const longitud = datos[0].lon;
-            console.log(datos[0].lon);
             setLatitud(latitud);
             setLongitud(longitud);
             setNombreCiudadApi(datos[0].name);
         } catch (error) {
             alert("Esa ciudad no existe");
-            console.log(error);
         }
     };
 
@@ -43,21 +39,16 @@ const Formulario = ({ kelvin_to_celsius }) => {
             );
             const datosClima = await peticionAPIclima.json();
             console.log("CONSULTA API CLIMA");
-            console.log(datosClima);
-            console.log(datosClima.main.temp);
             let temperatura = datosClima.main.temp;
             temperatura = Math.round(temperatura);
-            console.log("TEMPERATURAAAAA", temperatura);
             setTemperatura(kelvin_to_celsius(temperatura));
         } catch (error) {
             console.log(error);
-            alert("Esa ciudad no existe");
         }
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Ejecuto prevent default");
 
         consultaAPI();
         consultaAPIClima();
@@ -65,10 +56,8 @@ const Formulario = ({ kelvin_to_celsius }) => {
     };
 
     const handleInputChange = (value) => {
-        console.log(value.target.value);
         const ciudad = value.target.value;
         setNombreCiudad(ciudad);
-        console.log("Entramos a value");
     };
     return (
         <>
